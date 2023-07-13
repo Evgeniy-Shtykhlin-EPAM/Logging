bbf29e7c524c6ceb02c6bd8c12b7ea111da9913b
Evgeniy-Shtykhlin-EPAM <Evgeniy_Shtykhlin@epam.com>
2023-07-13 19:00:28 +0600

    Add ability to send logs by email

--- BrainstormSessions/Program.cs
+++ BrainstormSessions/Program.cs
@@ -1,8 +1,12 @@
 using System;
+using System.Net;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Json;
+using Serilog.Sinks.Email;
 
 namespace BrainstormSessions
 {
@@ -16,7 +20,21 @@ namespace BrainstormSessions
 
             Log.Logger = new LoggerConfiguration()
                 .ReadFrom.Configuration(configuration)
-                //.WriteTo.File(new JsonFormatter(),@"c:\temp\logs.log",shared: true)
+                .WriteTo.File(new JsonFormatter(),@"c:\temp\logs.log",shared: true)
+                .WriteTo.Email(new EmailConnectionInfo
+                {
+                    FromEmail = "from",
+                    ToEmail = "to",
+                    MailServer = "smtp.mail.ru",
+                    NetworkCredentials = new NetworkCredential
+                    {
+                        UserName = "UserName@mail.ru",
+                        Password = "password"
+                    },
+                    EnableSsl = true,
+                    Port = 465,
+                    EmailSubject = "Error in app"
+                }, restrictedToMinimumLevel: LogEventLevel.Debug, batchPostingLimit: 1)
                 .CreateLogger();
 
             try
